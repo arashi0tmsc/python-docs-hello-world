f0a7d24a348bfda505a6a710b6a032244bb9ec4c
arashi0tmsc <85921804+arashi0tmsc@users.noreply.github.com>
2021-06-24 09:43:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask
+
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
@@ -13,4 +16,26 @@ AZUE_KEY_VAULT_URL = "https://arashi22690kv02.vault.azure.net/"
 @app.route("/")
 def hello():
     return "Hello, Azure!"
-    
+
+@app.route('/secret')
+def index():
+    """Fetch secret from Azure Key Vault
+
+    Returns:
+        str: Response message to be displayed.
+    """
+    # Use Default credential. This has fallback authentication mechanism
+    credential = DefaultAzureCredential()
+    # Authenticate to Azure Key Vault.
+    secret_client = SecretClient(vault_url=AZUE_KEY_VAULT_URL, credential=credential)
+    # Fetch secrets created in Azure Key Vault
+    secrets = secret_client.get_secret('arashi22690testkey')
+    name = secrets.name
+    value = secrets.value
+    # Show secret name and it's value in the response
+    response = f'secret {name} value is {value}'
+    return response
+
+##
+if __name__ == "__main__":
+    app.run(debug=True)
